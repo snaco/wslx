c65502172e1cb8251f4dc9b575190dfd85a393ed
James Anderson <janderson5@albany.edu>
2018-10-05 16:51:42 -0400

    Added other Distros to the Setup.
    they should all work assuming you set up i3 correctly and exported DISPLAY correctly

--- WSLXSetup/WSLXSetup/Form1.Designer.cs
+++ WSLXSetup/WSLXSetup/Form1.Designer.cs
@@ -52,7 +52,7 @@
             "VcXsrv"});
 			this.xserver_client.Location = new System.Drawing.Point(12, 24);
 			this.xserver_client.Name = "xserver_client";
-			this.xserver_client.Size = new System.Drawing.Size(121, 24);
+			this.xserver_client.Size = new System.Drawing.Size(153, 24);
 			this.xserver_client.TabIndex = 0;
 			this.xserver_client.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
 			// 
@@ -61,9 +61,9 @@
 			this.button1.BackColor = System.Drawing.Color.Turquoise;
 			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-			this.button1.Location = new System.Drawing.Point(12, 54);
+			this.button1.Location = new System.Drawing.Point(138, 97);
 			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(121, 51);
+			this.button1.Size = new System.Drawing.Size(153, 51);
 			this.button1.TabIndex = 1;
 			this.button1.Text = "Generate Config";
 			this.button1.UseVisualStyleBackColor = false;
@@ -84,10 +84,12 @@
 			this.wsl_distro.FormattingEnabled = true;
 			this.wsl_distro.Items.AddRange(new object[] {
             "Ubuntu 18.04",
-            "Debian GNU/Linux"});
-			this.wsl_distro.Location = new System.Drawing.Point(139, 24);
+            "Debian GNU/Linux",
+            "openSUSE Leap 42",
+            "Kali"});
+			this.wsl_distro.Location = new System.Drawing.Point(170, 24);
 			this.wsl_distro.Name = "wsl_distro";
-			this.wsl_distro.Size = new System.Drawing.Size(121, 24);
+			this.wsl_distro.Size = new System.Drawing.Size(182, 24);
 			this.wsl_distro.TabIndex = 3;
 			// 
 			// window_manager
@@ -96,7 +98,7 @@
 			this.window_manager.FormattingEnabled = true;
 			this.window_manager.Items.AddRange(new object[] {
             "i3"});
-			this.window_manager.Location = new System.Drawing.Point(266, 24);
+			this.window_manager.Location = new System.Drawing.Point(358, 24);
 			this.window_manager.Name = "window_manager";
 			this.window_manager.Size = new System.Drawing.Size(121, 24);
 			this.window_manager.TabIndex = 4;
@@ -104,7 +106,7 @@
 			// distro_label
 			// 
 			this.distro_label.AutoSize = true;
-			this.distro_label.Location = new System.Drawing.Point(136, 4);
+			this.distro_label.Location = new System.Drawing.Point(167, 4);
 			this.distro_label.Name = "distro_label";
 			this.distro_label.Size = new System.Drawing.Size(79, 17);
 			this.distro_label.TabIndex = 5;
@@ -113,7 +115,7 @@
 			// windowmgr_label
 			// 
 			this.windowmgr_label.AutoSize = true;
-			this.windowmgr_label.Location = new System.Drawing.Point(263, 4);
+			this.windowmgr_label.Location = new System.Drawing.Point(355, 4);
 			this.windowmgr_label.Name = "windowmgr_label";
 			this.windowmgr_label.Size = new System.Drawing.Size(117, 17);
 			this.windowmgr_label.TabIndex = 6;
@@ -124,12 +126,13 @@
 			this.get_dep_btn.BackColor = System.Drawing.Color.SpringGreen;
 			this.get_dep_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.get_dep_btn.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-			this.get_dep_btn.Location = new System.Drawing.Point(266, 54);
+			this.get_dep_btn.Location = new System.Drawing.Point(12, 97);
 			this.get_dep_btn.Name = "get_dep_btn";
 			this.get_dep_btn.Size = new System.Drawing.Size(121, 51);
 			this.get_dep_btn.TabIndex = 7;
 			this.get_dep_btn.Text = "Install WM";
 			this.get_dep_btn.UseVisualStyleBackColor = false;
+			this.get_dep_btn.Click += new System.EventHandler(this.get_dep_btn_Click);
 			// 
 			// get_dep_tip
 			// 
@@ -140,9 +143,10 @@
 			// 
 			this.exec_btn.BackColor = System.Drawing.Color.Firebrick;
 			this.exec_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-			this.exec_btn.Location = new System.Drawing.Point(140, 54);
+			this.exec_btn.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.exec_btn.Location = new System.Drawing.Point(297, 97);
 			this.exec_btn.Name = "exec_btn";
-			this.exec_btn.Size = new System.Drawing.Size(120, 51);
+			this.exec_btn.Size = new System.Drawing.Size(182, 51);
 			this.exec_btn.TabIndex = 8;
 			this.exec_btn.Text = "Exit and Run WSLX";
 			this.exec_btn.UseVisualStyleBackColor = false;
@@ -153,7 +157,7 @@
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.SystemColors.WindowText;
-			this.ClientSize = new System.Drawing.Size(400, 120);
+			this.ClientSize = new System.Drawing.Size(489, 160);
 			this.Controls.Add(this.exec_btn);
 			this.Controls.Add(this.get_dep_btn);
 			this.Controls.Add(this.windowmgr_label);
--- WSLXSetup/WSLXSetup/Form1.cs
+++ WSLXSetup/WSLXSetup/Form1.cs
@@ -25,41 +25,71 @@ namespace WSLXSetup
 
 		private void button1_Click(object sender, EventArgs e)
 		{
-			string distro = wsl_distro.Items[wsl_distro.SelectedIndex].ToString();
-			string xserver = xserver_client.Items[xserver_client.SelectedIndex].ToString();
-			string win_mgr = window_manager.Items[window_manager.SelectedIndex].ToString();
-			Console.WriteLine(xserver);
-			switch (xserver)
+			if (wsl_distro.SelectedIndex == -1 || xserver_client.SelectedIndex == -1 || window_manager.SelectedIndex == -1)
 			{
-				case "VcXsrv":
-					xserver = "\"C:\\Program Files\\VcXsrv\\vcxsrv.exe\"";
-					break;
-				default:
-					break;
+				string selections = "";
+				if (wsl_distro.SelectedIndex == -1) selections += " * Linux Distro\n";
+				if (xserver_client.SelectedIndex == -1) selections += " * XServer Client\n";
+				if (window_manager.SelectedIndex == -1) selections += " * Window Manager";
+				MessageBox.Show("Missing selections:\n" + selections, "WSLX Setup", MessageBoxButtons.OK, MessageBoxIcon.Error);
 			}
-			switch (distro)
+			else
 			{
-				case "Ubuntu 18.04":
-					distro = "ubuntu1804.exe";
-					break;
-				case "Debian GNU/Linux":
-					distro = "debian.exe";
-					break;
-				default:
-					break;
+				string distro = wsl_distro.Items[wsl_distro.SelectedIndex].ToString();
+				string xserver = xserver_client.Items[xserver_client.SelectedIndex].ToString();
+				string win_mgr = window_manager.Items[window_manager.SelectedIndex].ToString();
+				Console.WriteLine(xserver);
+				switch (xserver)
+				{
+					case "VcXsrv":
+						xserver = "\"C:\\Program Files\\VcXsrv\\vcxsrv.exe\"";
+						break;
+					default:
+						break;
+				}
+				switch (distro)
+				{
+					case "Ubuntu 18.04":
+						distro = "ubuntu1804.exe";
+						break;
+					case "Debian GNU/Linux":
+						distro = "debian.exe";
+						break;
+					case "openSUSE Leap 42":
+						distro = "openSUSE-42.exe";
+						break;
+					case "Kali":
+						distro = "kali";
+						break;
+					default:
+						break;
+				}
+				xserver = "xserver_client=" + xserver;
+				distro = "distro=" + distro;
+				win_mgr = "window_manager=" + win_mgr;
+				string[] lines = { xserver, distro, win_mgr };
+				string config_file = System.IO.Path.GetDirectoryName(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName) + @"\config";
+				System.IO.File.WriteAllLines(config_file, lines);
 			}
-			xserver = "xserver_client=" + xserver;
-			distro = "distro=" + distro;
-			win_mgr = "window_manager=" + win_mgr;
-			string[] lines = {xserver, distro, win_mgr };
-			string config_file = System.IO.Path.GetDirectoryName(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName) + @"\config";
-			System.IO.File.WriteAllLines(config_file, lines);
 		}
 
 		private void exec_btn_Click(object sender, EventArgs e)
 		{
 			string wslx_loc = System.IO.Path.GetDirectoryName(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName) + @"\WSLX.exe";
 			System.Diagnostics.Process.Start(wslx_loc);
+			this.Close();
+		}
+
+		private void get_dep_btn_Click(object sender, EventArgs e)
+		{
+			if (wsl_distro.SelectedIndex == -1 || window_manager.SelectedIndex == -1)
+			{
+				string selections = "";
+				if (wsl_distro.SelectedIndex == -1) selections += " * Linux Distro\n";
+				if (xserver_client.SelectedIndex == -1) selections += " * XServer Client\n";
+				if (window_manager.SelectedIndex == -1) selections += " * Window Manager";
+				MessageBox.Show("Missing selections:\n" + selections, "WSLX Setup", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
 		}
 	}
 }
